2b0247330b22734ecf065e7657353c6290b35bbc
SpllintteR <pauloweber@hotmail.com.br>
2018-09-15 12:58:40 -0300

    adding parameters

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,25 @@
 var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: "React",
+            message: "This is a form component" 
+        };
+    },
     render: function() {
+        var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
-                <h1>Hello React component</h1>
-                <p>component test ok</p>
+                <h1>Hello React {name}</h1>
+                <p>{message + '!!'}</p>
             </div>
         );
     }
 });
 
+var firstName = "Paulo";
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message="a beautiful and meaningful message"/>,
     document.getElementById('app')
 );
