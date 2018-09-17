703e979500ea43e3c896f071c2f8d0ef0e165fbc
paulo.weber <paulo.weber@senior.com.br>
2018-09-17 13:35:58 -0300

    updating state

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,34 @@
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {
-            name: "React",
-            message: "This is a form component" 
+            name: "React"
         };
     },
+    getInitialState: function () {
+        return {
+            name: this.props.name
+        }
+    },
+    onButtonClick: function(e) {
+        e.preventDefault();
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
+        if(typeof name === 'string' && name.length > 0){
+            this.setState({
+                name: name
+            });
+        }
+    },
     render: function() {
-        var name = this.props.name;
-        var message = this.props.message;
+        var name = this.state.name;
         return (
             <div>
                 <h1>Hello React {name}</h1>
-                <p>{message + '!!'}</p>
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         );
     }
@@ -20,6 +37,6 @@ var Greeter = React.createClass({
 var firstName = "Paulo";
 
 ReactDOM.render(
-    <Greeter name={firstName} message="a beautiful and meaningful message"/>,
+    <Greeter name={firstName}/>,
     document.getElementById('app')
 );
