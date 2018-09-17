6504ffb8dd1690fade4e9b29cb5cbee89e796258
paulo.weber <paulo.weber@senior.com.br>
2018-09-17 14:17:27 -0300

    section 3 Challenge

--- public/app.jsx
+++ public/app.jsx
@@ -15,16 +15,24 @@ var GreeterForm = React.createClass({
     onFormSubmit: function(e) {
         e.preventDefault();
         var name = this.refs.name.value;
+        var message = this.refs.message.value;
+        var updates = {}
         if(name.length > 0){
+            updates.name = name;
             this.refs.name.value = '';
-            this.props.onNewName(name);
         }
+        if(message.length > 0){
+            updates.message = message;
+            this.refs.message.value = '';
+        }
+        this.props.onNewData(updates);
     },
     render: function() {
        return (
             <form onSubmit={this.onFormSubmit}>
-                <input type="text" ref="name"/>
-                <button>Set Name</button>
+                <div><input type="text" ref="name" placeholder="Enter Name"/></div>
+                <div><textarea ref="message" placeholder="Enter Message"/></div>
+                <div><button>Submit</button></div>
             </form>
        );
     }
@@ -37,23 +45,22 @@ var Greeter = React.createClass({
             message: "This is the default message!"
         };
     },
-    handleNewName: function(name) {
-        this.setState({
-            name: name
-        });
+    handleNewData: function(updates) {
+        this.setState(updates);
     },
     getInitialState: function () {
         return {
-            name: this.props.name
+            name: this.props.name,
+            message: this.props.message
         }
     },
     render: function() {
         var name = this.state.name;
-        var message = this.props.message;
+        var message = this.state.message;
         return (
             <div>
                 <GreeterMessage name={name} message={message}/>
-                <GreeterForm onNewName={this.handleNewName}/>
+                <GreeterForm onNewData={this.handleNewData}/>
             </div>
         );
     }
